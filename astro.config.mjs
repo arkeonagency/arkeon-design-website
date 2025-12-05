0924b88d327afeb75e2d03628f3225526df6f45a
arkeonagency <btadele009@gmail.com>
2025-12-05 07:28:28 +0000

    v15

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
+import keystatic from '@keystatic/astro';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  // REPLACE WITH YOUR ACTUAL VERCEL URL
   site: 'https://arkeon-studio.vercel.app', 
   
-  integrations: [react(), tailwind({
-    applyBaseStyles: false,
-  }), sitemap()],
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+  ],
   output: 'static',
 });
